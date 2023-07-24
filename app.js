5d120ed1141621a96edc0660796d5af747aa7c5d
Leonardo B. Alcaras <138508225+leonardobdev@users.noreply.github.com>
2023-07-24 00:03:07 -0300

    .

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
 if (navigator.serviceWorker) {
-  navigator.serviceWorker.register('/leonardob.dev/sw.js', { scope: '/leonardob.dev/' })
+  navigator.serviceWorker.register('https://leonardobdev.github.io/leonardob.dev/sw.js', { scope: 'https://leonardobdev.github.io/leonardob.dev/' })
 }
